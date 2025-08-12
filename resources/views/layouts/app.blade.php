27d0c3eb5faa7fb369108489f6eed5738eb0f167
rajeshraju97 <rajesh.teppala97@gmail.com>
2025-08-12 13:11:26 +0530

    updated frontend user dashboard and favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <meta content="" name="keywords" />
     <meta content="" name="description" />
+    <link rel="icon" type="image/png" href="{{ asset('img/hero4.png') }}">
+
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
